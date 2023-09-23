097a306e300256d7d25f069bba85e48f03d17a8b
Evgeniy <blbka@bk.ru>
2023-09-23 03:10:39 +0300

    исправление2

--- home_work4.py
+++ home_work4.py
@@ -2,25 +2,15 @@
 # два числа A и B, и возводит число А в целую степень B с
 # помощью рекурсии.
 
-# def step(a, b, count , res):
+# def step(a,b):
 #     if b == 0:
 #         return 1
-#     if b == 1:
-#         return a
-#     if count == 0:
-#         res = a * a
-#         count += 1
-#         return step(a, b, count, res)
-#     elif count != (b-1) and count > 0:
-#         res = res * a
-#         count += 1
-#         return step(a, b, count, res)
-#     else:
-#         return res
+#     return a * step(a,b - 1) 
     
 # a = int(input())
 # b = int(input())
-# print(step(a, b, count = 0, res = 0))
+
+# print(step(a,b))
 
 # Задача 28: Напишите рекурсивную функцию sum(a, b),
 # возвращающую сумму двух целых неотрицательных чисел. Из
